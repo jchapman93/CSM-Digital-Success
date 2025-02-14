73a20b54280d5f0e0e5ae485a02fa57024143217
jchapman93 <jessie.chapman01@gmail.com>
2025-02-14 04:23:32 +0000

    Added google drive links

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://drive.google.com/file/d/1XPLuCCk5E8IeMmWa_ka9HZ-olM86AS_g/view?usp=sharing"
+            href="https://drive.google.com/file/d/1ETkeT-NLkNuoLLj7qdZ5-nt2DasVM0JT/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -32,11 +32,11 @@ export default function Home() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://drive.google.com/file/d/1JHi9m0S7PLlePRlUaak4bgI-aMFfbV30/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            README and Reflection
           </a>
         </div>
       </main>
